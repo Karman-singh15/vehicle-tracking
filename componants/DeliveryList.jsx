3ead9bb96a39eaaaa7010287f4a2ad119e58f39a
Karman Singh <kswgeneral@gmail.com>
2025-03-06 17:11:00 +0530

    Update DeliveryList.jsx

--- componants/DeliveryList.jsx
+++ componants/DeliveryList.jsx
@@ -2,7 +2,8 @@ import RemoveBtn from "./RemoveBtn";
 
 const getDelivery = async () => {
     try {
-        const res = await fetch(`/api/vehicles`, {
+        const res = await fetch(`https://vehicle-tracking-five.vercel.app/api/vehicles`, {
+            method: "GET"
             cache: "no-store"
         });
 
